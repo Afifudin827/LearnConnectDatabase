faccdcc6f9c4ad24280e59c1df61c33353297616
Afifudin <afifudin.card@gmail.com>
2023-09-20 09:33:37 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,251 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+
+namespace BasicConnectivity;
+
+class Program
+{
+    static string dataBase = "Database=db_hr;Data Source=AFIFWORKS\\MSSQLSERVER01;Integrated Security=True;Connect Timeout=30;";
+    static SqlConnection conn;
+    public static void Main()
+    {
+        //InsertRegion(6, "Nort Asia");
+        //GetRegionById(1);
+        //UpdateRegion(1, "Asians");
+        //DeleteRegion(6);
+    }
+
+    // GET ALL: Region
+    public static void GetAllRegions()
+    {
+        using var connection = new SqlConnection(dataBase);
+        using var command = new SqlCommand();
+
+        command.Connection = connection;
+        command.CommandText = "SELECT * FROM regions";
+
+        try
+        {
+            connection.Open();
+
+            using var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+                while (reader.Read())
+                {
+                    Console.WriteLine("Id: " + reader.GetInt32(0));
+                    Console.WriteLine("Name: " + reader.GetString(1));
+                }
+            else
+                Console.WriteLine("No rows found.");
+
+            reader.Close();
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    // GET BY ID: Region
+    public static void GetRegionById(int id)
+    {
+        using var connection = new SqlConnection(dataBase);
+        using var command = new SqlCommand();
+
+        command.Connection = connection;
+        command.CommandText = "SELECT * FROM tbl_regions where id = @PId";
+        try
+        {
+            var pId = new SqlParameter();
+            pId.ParameterName = "@PId";
+            pId.Value = id;
+            pId.SqlDbType = SqlDbType.VarChar;
+            command.Parameters.Add(pId);
+
+            connection.Open();
+            using var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                Console.WriteLine("Id: " + reader.GetInt32(0));
+                Console.WriteLine("Name: " + reader.GetString(1));
+            }
+            else
+            {
+                Console.WriteLine("No rows found.");
+            }
+
+            reader.Close();
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    // INSERT: Region
+    public static void InsertRegion(int id,string name)
+    {
+        using var connection = new SqlConnection(dataBase);
+        using var command = new SqlCommand();
+
+        command.Connection = connection;
+        command.CommandText = "INSERT INTO tbl_regions VALUES (@id,@name);";
+
+        try
+        {
+            var pId = new SqlParameter();
+            pId.ParameterName = "@id";
+            pId.Value = id;
+            pId.SqlDbType = SqlDbType.VarChar;
+            command.Parameters.Add(pId);
+
+            var pName = new SqlParameter();
+            pName.ParameterName = "@name";
+            pName.Value = name;
+            pName.SqlDbType = SqlDbType.VarChar;
+            command.Parameters.Add(pName);
+
+            connection.Open();
+            using var transaction = connection.BeginTransaction();
+            try
+            {
+                command.Transaction = transaction;
+
+                var result = command.ExecuteNonQuery();
+
+                transaction.Commit();
+                connection.Close();
+
+                switch (result)
+                {
+                    case >= 1:
+                        Console.WriteLine("Insert Success");
+                        break;
+                    default:
+                        Console.WriteLine("Insert Failed");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                Console.WriteLine($"Error Transaction: {ex.Message}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    // UPDATE: Region
+    public static void UpdateRegion(int id, string name) 
+    {
+        using var connection = new SqlConnection(dataBase);
+        using var command = new SqlCommand();
+
+        command.Connection = connection;
+        command.CommandText = "UPDATE tbl_regions set name = @name where id = @id;";
+
+        try
+        {
+            var pName = new SqlParameter();
+            pName.ParameterName = "@name";
+            pName.Value = name;
+            pName.SqlDbType = SqlDbType.VarChar;
+            command.Parameters.Add(pName);
+
+            var pId = new SqlParameter();
+            pId.ParameterName = "@id";
+            pId.Value = id;
+            pId.SqlDbType = SqlDbType.VarChar;
+            command.Parameters.Add(pId);
+
+            connection.Open();
+            using var transaction = connection.BeginTransaction();
+            try
+            {
+                command.Transaction = transaction;
+
+                var result = command.ExecuteNonQuery();
+
+                transaction.Commit();
+                connection.Close();
+
+                switch (result)
+                {
+                    case >= 1:
+                        Console.WriteLine("Update Success");
+                        break;
+                    default:
+                        Console.WriteLine("Update Failed");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                Console.WriteLine($"Error Transaction: {ex.Message}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    // DELETE: Region
+    public static void DeleteRegion(int id) 
+    {
+        using var connection = new SqlConnection(dataBase);
+        using var command = new SqlCommand();
+
+        command.Connection = connection;
+        command.CommandText = "DELETE FROM tbl_regions where id = @id;";
+
+        try
+        {
+            var pId = new SqlParameter();
+            pId.ParameterName = "@id";
+            pId.Value = id;
+            pId.SqlDbType = SqlDbType.VarChar;
+            command.Parameters.Add(pId);
+
+            connection.Open();
+            using var transaction = connection.BeginTransaction();
+            try
+            {
+                command.Transaction = transaction;
+
+                var result = command.ExecuteNonQuery();
+
+                transaction.Commit();
+                connection.Close();
+
+                switch (result)
+                {
+                    case >= 1:
+                        Console.WriteLine("Delete Success");
+                        break;
+                    default:
+                        Console.WriteLine("Delete Failed");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                Console.WriteLine($"Error Transaction: {ex.Message}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
